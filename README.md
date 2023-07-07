11bf5da8a8da205685783c42aed3cdf621503fe5
jrk <jrk@debian-BULLSEYE-live-builder-AMD64>
2023-07-07 12:15:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,178 @@
+# simpleble2mqtt
+
+## MQTT Bluetooth Low Energy beacon tracker
+
+Every time a BLE beacon is received, it applies a Kalman filter to smooth the RSSI and reduce the noise to make the system more accurate.
+
+[Home Assistant](https://www.home-assistant.io/) compatible.
+
+## Prerequisites
+
+- On Debian based machines:
+```
+apt-get install python3-pip python3-setuptools python3-dev libglib2.0-dev build-essential git
+```
+
+- Clone the repository
+```
+git clone https://github.com/jr-k/simpleble2mqtt.git
+cd simpleble2mqtt
+```
+
+- Install Python dependencies:
+```
+pip3 install -r requirements.txt
+```
+- Check bluetooth device state:
+```
+sudo hciconfig hci0 down && sudo hciconfig hci0 up
+```
+If you receive `Can't init device hci0: Operation not possible due to RF-kill (132)` execute:
+```
+connmanctl enable bluetooth
+```
+
+## General setup
+
+### Calibration
+
+**IMPORTANT**: Before running any scan, take the BLE device 1 meter away from the BLE scanner device.
+
+To discover devices as well as calculate the "**MR**" and the "**N**" constant, run the script with the following arguments:
+
+```
+python3 simpleble2mqtt.py -s -d [device-mac-here]
+```
+
+***Note**: To see all the devices in range, remove the "--device [device-mac-here]" argument
+
+Let that scan run for a while, and once the "distance" variable is close to 1 meter and "stable" between different samples, stop the scan by pressing "CTRL+C" and write down the "**MR**" and "**N**" variables.
+
+Sample output:
+```
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-67 dB, MR=-0.1671, N=4.90251674, Distance=23.08
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-75 dB, MR=-25.3183, N=4.15764916, Distance=15.67
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-77 dB, MR=-38.4631, N=3.67363437, Distance=11.19
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-77 dB, MR=-46.3992, N=3.32897478, Distance=8.30
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-72 dB, MR=-50.8587, N=2.91815640, Distance=5.30
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-77 dB, MR=-54.8311, N=2.96278122, Distance=5.60
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-76 dB, MR=-57.7031, N=2.79462608, Distance=4.52
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-67 dB, MR=-58.8496, N=2.35396926, Distance=2.22
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-76 dB, MR=-60.8000, N=2.66012685, Distance=3.73
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-72 dB, MR=-61.9886, N=2.43478816, Distance=2.58
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-67 dB, MR=-63.3710, N=2.15760607, Distance=1.47
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-72 dB, MR=-64.1557, N=2.34067466, Distance=2.16
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-67 dB, MR=-64.4047, N=2.11271405, Distance=1.33
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-77 dB, MR=-65.4715, N=2.50067683, Distance=2.89
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-76 dB, MR=-66.3382, N=2.41960843, Distance=2.51
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-67 dB, MR=-69.6263, N=2.11405924, Distance=0.75
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-71 dB, MR=-69.5530, N=2.06284109, Distance=1.18
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-71 dB, MR=-69.6518, N=2.05855109, Distance=1.16
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-67 dB, MR=-69.4929, N=2.10826675, Distance=0.76
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-67 dB, MR=-69.3228, N=2.10087613, Distance=0.78
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-67 dB, MR=-69.1642, N=2.09399012, Distance=0.79
+ca:fe:ca:fe:ca:fe (BLE Device), RSSI=-70 dB, MR=-69.6756, N=2.01408935, Distance=1.04
+```
+
+In this case, let's write down:
+ - **MR**: -69.6756
+ - **N**: 2.01408935
+
+## Configuration
+
+Before running the BLE tracker, you need to configure some settings in `config.yaml`.
+Run `cp config.yaml.dist config.yaml`
+
+```yaml
+interval: 5                     # Scan interval in seconds
+status_interval_threshold: 3    # Max interval to trigger status change [status_interval_threshold * interval] seconds
+mqtt:
+  broker: host                  # Mqtt server host
+  port: 1883                    # Mqtt server port
+  username:                     # Mqtt server username (remove key if unused)
+  password:                     # Mqtt server password (remove key if unused)
+  client_id:                    # Unique client id
+  topic: ble_tag/status         # Mqtt root topic
+devices:
+  - address: xx:xx:xx:xx:xx:xx  # Mac address of ble tag
+    topic: user1                # Subtopic for this device
+    payload: tag1               # Custom payload injected in JSON 
+    ble:        
+      MR: -77.8746              # Measured RSSI obtained with calibration
+      N: 2.00544811             # N obtained with calibration
+```
+
+## Installation
+
+1. Move the project folder to your prefered location
+2. Edit the `simpleble2mqtt.service` and set the right paths to `WorkingDirectory` and `ExecStart`
+3. Adjust your settings in `config.yaml`
+4. Install the service
+```
+cp simpleble2mqtt.service /etc/systemd/system/
+systemctl enable simpleble2mqtt
+```
+5. You're ready to go
+```
+systemctl start simpleble2mqtt
+```
+
+## Message format
+
+### Detected
+
+```bash
+# topic: ble_tracker/status/user1
+{"detected": true, "distance": 1.14, "rssi": -79, "payload": "tag1"}
+```
+
+### Not detected
+```bash
+# topic: ble_tracker/status/user1
+{"detected": false, "distance": -1, "rssi": null, "payload": "tag2"}
+```
+
+
+## Home Assistant integration
+
+### Tracker detection entity
+
+```
+sensor:
+  - platform: mqtt_room
+    name: ble tag 1
+    device_id: ca:fe:ca:fe:ca:fe
+    state_topic: bletracker
+    timeout: 60
+    away_timeout: 360
+```
+
+[Home Assistant sensor.mqtt_room](https://www.home-assistant.io/integrations/mqtt_room/)
+
+### Binary detection entity
+
+```
+binary_sensor:
+  - platform: mqtt
+    name: Tracked device on
+    state_topic: "bletracker/livingroom/bletag1"
+    value_template: "{{ value_json.in_range }}"
+    payload_on: "True"
+    payload_off: "False"
+    off_delay: 120
+```
+
+[Home Assistant binary_sensor.mqtt](https://www.home-assistant.io/integrations/binary_sensor.mqtt/)
+
+## Kalman Filter
+
+- [PyConAu2016](https://pyvideo.org/events/pycon-au-2016.html) - [Working with real-time data streams in Python](https://www.youtube.com/watch?v=gFeTkB8VHpw)
+- GitHub repository (Lian Blackhall) - https://github.com/lblackhall/pyconau2016
+
+## Related projects
+
+- https://github.com/sch3m4/mqtt-ble-tracker
+- https://github.com/mKeRix/room-assistant
+- https://github.com/happy-bubbles/presence
+- https://jptrsn.github.io/ESP32-mqtt-room/
+- https://github.com/1technophile/OpenMQTTGateway
---
+++ simpleble2mqtt.py
@@ -0,0 +1,204 @@
+#!/usr/bin/python3
+
+import asyncio
+import time
+import math
+import yaml
+import json
+import signal
+import argparse
+import threading
+import sys
+import os
+
+from bleak import BleakScanner
+from paho.mqtt import client as mqtt_client
+
+#####################################
+#       Helper Classes
+#####################################
+
+class SingleStateKalmanFilter():
+    def __init__(self, A, B, C, x, P, Q, R):
+        self.__A = A
+        self.__B = B
+        self.__C = C
+        self.__current_state_estimate = x
+        self.__current_prob_estimate = P
+        self.__Q = Q
+        self.__R = R
+
+    def current_state(self):
+        return self.__current_state_estimate
+
+    def step(self, control_input, measurement):
+        # prediction step
+        predicted_state_estimate = self.__A * self.__current_state_estimate + self.__B * control_input
+        predicted_prob_estimate = (self.__A * self.__current_prob_estimate) * self.__A + self.__Q
+
+        # innovation step
+        innovation = measurement - self.__C * predicted_state_estimate
+        innovation_covariance = self.__C * predicted_prob_estimate * self.__C + self.__R
+
+        # update step
+        kalman_gain = predicted_prob_estimate * self.__C * 1 / float(innovation_covariance)
+        self.__current_state_estimate = predicted_state_estimate + kalman_gain * innovation
+
+        # eye(n) = nxn identity matrix
+        self.__current_prob_estimate = (1 - kalman_gain * self.__C) * predicted_prob_estimate
+
+#####################################
+#       Configuration Setup
+#####################################
+
+if not os.path.isfile("config.yaml"):
+    print("Configuration file config.yaml does not exist, copy from config.yaml.dist then run again")
+    sys.exit(1)
+
+# Load configuration from config.yaml
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+
+# MQTT configuration
+mqtt_config = config["mqtt"]
+client = mqtt_client.Client(mqtt_config["client_id"])
+if "username" in mqtt_config and "password" in mqtt_config:
+    client.username_pw_set(mqtt_config["username"], mqtt_config["password"])
+client.connect(mqtt_config["broker"], mqtt_config["port"])
+
+# Beacon configuration
+devices_config = {device["address"]: device for device in config["devices"]}
+devices_to_track = [device.lower() for device in devices_config.keys()]
+tag_detected = {device: False for device in devices_to_track}
+rssi_values = {device: None for device in devices_to_track}
+status_interval_threshold = {device: 0 for device in devices_to_track}
+
+#####################################
+#       ACTION: Calibrate
+#####################################
+
+class Calibrator():
+    def __init__(self):
+        self.scanner = BleakScanner()
+
+    async def scan(self, period=10):
+        await asyncio.sleep(period)  # to simulate the scan period
+        return await self.scanner.discover()
+
+    def get_n(self, mr, rssi, distance=1.0):
+        return 2 + abs(((mr - rssi) * math.log(math.e, 10)) / 10)
+
+    def get_distance(self, rssi, mr, n):
+        if n == 0:
+            return -1
+        return math.pow(10, (mr - rssi) / (10 * n))
+
+async def calibrate(maclist):
+    scanner = Calibrator()
+    devlist = {}
+    calibrator_period = 10
+
+    while True:
+        devices = await scanner.scan(period=calibrator_period)
+        for dev in devices:
+            if len(maclist) > 0 and dev.address.lower() not in maclist:
+                continue
+
+            rssi = dev.rssi
+
+            if dev.address.lower() not in devlist.keys():
+                a = 1  # no process innovation
+                c = 1  # measurement
+                b = 0  # no control input
+                q = 0.005  # process covariance
+                r = 1  # measurement covariance
+                x = rssi  # initial estimate
+                p = 1  # initial covariance
+                devlist[dev.address.lower()] = SingleStateKalmanFilter(a, b, c, x, p, q, r)
+
+            devlist[dev.address.lower()].step(0, abs(rssi))
+
+            # calculate distance with the smoothed RSSI
+            frssi = -1 * devlist[dev.address.lower()].current_state()
+            n = scanner.get_n(frssi, rssi)
+            dist = scanner.get_distance(rssi, frssi, n)
+            print("{} ({}), RSSI={} dB, MR={:.4f}, N={:.8f}, Distance={:.2f}".format(dev.address.lower(), dev.name, rssi, frssi, n, dist))
+    await asyncio.sleep(calibrator_period + 3)
+
+#####################################
+#       ACTION: Scan
+#####################################
+
+async def scan(verbose):
+    scanner = BleakScanner(detection_callback=detection_callback)
+
+    while True:
+        devices = await scanner.start()
+        await asyncio.sleep(config["interval"])
+        await scanner.stop()
+
+        for device in devices_to_track:
+            device = device.lower()
+            subtopic = devices_config[device]['topic']
+
+            # Calculate distance and prepare message
+            message = {
+                "detected": tag_detected[device],
+                "distance": -1 if not tag_detected[device] else round(calculate_distance(device, rssi_values[device]), 2),
+                "rssi": rssi_values[device],
+                "payload": devices_config[device]['payload']
+            }
+
+            # Check if device has been detected
+            if tag_detected[device]:
+                if verbose:
+                    print(f"Device {device} ({subtopic}) detected at distance: {message['distance']} meters")
+                client.publish(f"{mqtt_config['topic']}/{subtopic}", json.dumps(message))
+                status_interval_threshold[device] = 0  # Reset status count if tag detected
+            else:
+                status_interval_threshold[device] += 1  # Increase status count
+                if status_interval_threshold[device] >= config["status_interval_threshold"]:
+                    if verbose:
+                        print(f"Device {device} ({subtopic}) not detected")
+                    client.publish(f"{mqtt_config['topic']}/{subtopic}", json.dumps(message))
+                    status_interval_threshold[device] = 0  # Reset status count after sending not detected message
+
+            tag_detected[device] = False
+            rssi_values[device] = None
+
+def detection_callback(device, advertisement_data):
+    device_address = device.address.lower()
+    # Check if detected device is in our beacon list
+    if device_address in devices_to_track:
+        tag_detected[device_address] = True
+        rssi_values[device_address] = advertisement_data.rssi
+
+def calculate_distance(device, rssi):
+    """ Calculate distance based on RSSI """
+    return 10 ** ((devices_config[device]["ble"]["MR"] - rssi) / (10 * devices_config[device]["ble"]["N"]))
+
+#####################################
+#       MAIN
+#####################################
+
+def main():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-c", "--calibrate", help="Start calibrate for devices showing the avearage RSSI 1 meter away", action="store_true")
+    group.add_argument("-s", "--scan", help="Start daemon for registered devices", action="store_true")
+    parser.add_argument("-d", "--device", nargs='+', help="Filter by device mac when scanning", default=[])
+    parser.add_argument("-v", "--verbose", help="Run in verbose mode", action="store_true", default=False)
+    args = parser.parse_args()
+
+    if args.scan:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(scan(args.verbose))
+    elif args.calibrate:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(calibrate(args.device))
+
+if __name__ == "__main__":
+    main()
+
+
+
